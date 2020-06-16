ec3d0104caa7298328705097ab4eb59947aa580c
Robert Luttig <robert.luttig@gmail.com>
2020-06-15 19:56:11 -0700

    added timer to script

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var timerEl = document.getElementById("countdown");
+
+  
+  function prepareRead() {
+    var timeLeft = 60;
+  
+    var timeInterval = setInterval(function () {
+      timerEl.textContent = timeLeft + "Time";
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        timerEl.textContent = "Game Over";
+        speedRead();
+      }
+    }, 1000);
+}
+prepareRead();
