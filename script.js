bef9b4f6c2024d5377ef6d467ae9827218afe22e
Robert Luttig <robert.luttig@gmail.com>
2020-06-17 18:05:32 -0700

    created first var Questions and started first code question by creating new p element

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 var timerEl = document.getElementById("countdown");
-var button = document.querySelector(".btn");
-var pElement = document.createElement("p");
-var inputEl = document.createElement("input");
+var startButton = document.querySelector("#btn");
 
-button.addEventListener("click",function(event) {
+var questions = [ 
+  {
+    question: "What is my first question?",
+    choices: [],
+    answer: "",
+  },
+  {
+    question: "What is my second question?",
+    choices: [],
+    answer: "",
+  }
+]
+
+startButton.addEventListener("click",function(event) {
     event.preventDefault();
   
     var timeLeft = 60;
@@ -16,7 +27,35 @@ button.addEventListener("click",function(event) {
       if (timeLeft === 0) {
         clearInterval(timeInterval);
         timerEl.textContent = "Game Over";
-        speedRead();
       }
     }, 1000);
+    codeQuestions();
 });
+
+console.log(questions[0].question);
+
+function codeQuestions(){
+  var pElement = document.createElement("p");
+  var inputEl = document.createElement("input");
+  var pMain = document.querySelector(".main");
+  pMain.appendChild(pElement)
+}
+
+//render quiz function
+//add class to div element
+//create p element
+// create text content "question"
+//create input elements with attributes append to p element
+//append p element to div element
+//call render quiz inside of eventlistener function line 23 
+
+// check user answer function
+//var useranswer
+//add eventlistern to answer
+//store user answer
+// if else check answer
+//if correct shows correct message
+//if wrong shows wrong message
+//deduct 10seconds from timer
+
+// call function inside of eventlistener function
